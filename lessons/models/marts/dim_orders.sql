87bd202a02a2a34a682dbef0fd31727c631dbb06
GrafVorfahrt <mr.prinz7@gmail.com>
2024-09-02 13:18:35 +0900

    Lesson update

--- lessons/models/marts/dim_orders.sql
+++ lessons/models/marts/dim_orders.sql
@@ -29,6 +29,10 @@ SELECT
 	om.total_profit,
 	om.total_discount,
 
+	TIMESTAMP_DIFF(od.created_at, user_data.first_order_created_at, DAY) AS days_since_first_order
+
 FROM {{ ref('stg_ecommerce__orders') }} AS od
 LEFT JOIN order_item_measures AS om
 	on od.order_id = om.order_id
+LEFT JOIN {{ ref('int_ecommerce__first_order_created') }} AS user_data
+	on user_data.user_id = od.user_id
